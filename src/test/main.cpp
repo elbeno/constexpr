58dbf6f357fc04f92127ae9bed1f9b6886e8d425
Ben Deane <ben@elbeno.com>
2015-08-22 12:13:46 -0700

    C++11 support and static_assert tests
    Work in progress

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -6,14 +6,40 @@
 
 using namespace std;
 
+//----------------------------------------------------------------------------
 // constexpr math functions
+
 namespace cex
 {
+  //----------------------------------------------------------------------------
+  constexpr float abs(float x)
+  {
+    return x >= 0 ? x : -x;
+  }
   constexpr double abs(double x)
   {
     return x >= 0 ? x : -x;
   }
+  constexpr long double abs(long double x)
+  {
+    return x >= 0 ? x : -x;
+  }
 
+  //----------------------------------------------------------------------------
+  constexpr float fabs(float x)
+  {
+    return x >= 0 ? x : -x;
+  }
+  constexpr double fabs(double x)
+  {
+    return x >= 0 ? x : -x;
+  }
+  constexpr long double fabs(long double x)
+  {
+    return x >= 0 ? x : -x;
+  }
+
+  //----------------------------------------------------------------------------
   constexpr double ipow(double x, int n)
   {
     return (n == 0) ? 1.0 :
@@ -22,83 +48,145 @@ namespace cex
       1.0 / ipow(x, -n);
   }
 
-  // sqrt by Newton's method
+  //----------------------------------------------------------------------------
+  // square root by Newton-Raphson method
+  namespace detail
+  {
+    template <typename T>
+    constexpr T sqrt(T x, T guess)
+    {
+      return (guess + x/guess)/2 == guess ?
+        guess :
+        sqrt(x, (guess + x/guess)/2);
+    }
+  }
+  constexpr float sqrt(float x)
+  {
+    return detail::sqrt(x, 1.0f);
+  }
   constexpr double sqrt(double x)
   {
-    double guess = 1.0;
-    double eps = std::numeric_limits<double>::epsilon() * 2.0;
-    while (abs(guess * guess - x) > eps)
-    {
-      guess = (guess + x/guess) / 2.0;
-    }
-    return guess;
+    return detail::sqrt(x, 1.0);
+  }
+  constexpr long double sqrt(long double x)
+  {
+    return detail::sqrt(x, 1.0l);
   }
 
-  // exp by taylor series expansion
+  //----------------------------------------------------------------------------
+  // cube root by Newton-Raphson method
+  namespace detail
+  {
+    template <typename T>
+    constexpr T cbrt(T x, T guess)
+    {
+      return (2*guess + x/(guess*guess))/3 == guess ?
+        guess :
+        cbrt(x, (2*guess + x/(guess*guess))/3);
+    }
+  }
+  constexpr float cbrt(float x)
+  {
+    return detail::cbrt(x, 1.0f);
+  }
+  constexpr double cbrt(double x)
+  {
+    return detail::cbrt(x, 1.0);
+  }
+  constexpr long double cbrt(long double x)
+  {
+    return detail::cbrt(x, 1.0l);
+  }
+
+  //----------------------------------------------------------------------------
+  // exp by Taylor series expansion
+  namespace detail
+  {
+    template <typename T>
+    constexpr T exp(T x, T sum, T n, int i, T t)
+    {
+      return sum + t/n == sum ?
+        sum :
+        exp(x, sum + t/n, n * i, i+1, t * x);
+    }
+  }
+  constexpr float exp(float x)
+  {
+    return detail::exp(x, 1.0f, 1.0f, 2, x);
+  }
   constexpr double exp(double x)
   {
-    double sum = 1.0;
-    double prevsum = 0;
-    double n = 1.0;
-    double t = x;
-    for (uint64_t i = 2; sum != prevsum; ++i)
-    {
-      prevsum = sum;
-      sum += t / n;
-      t *= x;
-      n *= i;
-    }
-    return sum;
+    return detail::exp(x, 1.0, 1.0, 2, x);
+  }
+  constexpr long double exp(long double x)
+  {
+    return detail::exp(x, 1.0l, 1.0l, 2, x);
   }
 
-  // sin by taylor series expansion
+  //----------------------------------------------------------------------------
+  // sin by Taylor series expansion
+  namespace detail
+  {
+    template <typename T>
+    constexpr T trig_series(T x, T sum, T n, int i, int s, T t)
+    {
+      return sum + t*s/n == sum ?
+        sum :
+        trig_series(x, sum + t*s/n, n*i*(i+1), i+2, -s, t*x*x);
+    }
+  }
+  constexpr float sin(float x)
+  {
+    return detail::trig_series(x, x, 6.0f, 4, -1, x*x*x);
+  }
   constexpr double sin(double x)
   {
-    double sum = x;
-    double prevsum = 0;
-    double n = 1.0;
-    double t = x;
-    double s = 1.0;
-    for (uint64_t i = 2; sum != prevsum; ++i)
-    {
-      s = -s;
-      t *= x * x;
-      n *= i; ++i; n *= i;
-      prevsum = sum;
-      sum += s*t / n;
-    }
-    return sum;
+    return detail::trig_series(x, x, 6.0, 4, -1, x*x*x);
+  }
+  constexpr long double sin(long double x)
+  {
+    return detail::trig_series(x, x, 6.0l, 4, -1, x*x*x);
   }
 
-  // cos by taylor series expansion
+  //----------------------------------------------------------------------------
+  // cos by Taylor series expansion
+  constexpr float cos(float x)
+  {
+    return detail::trig_series(x, 1.0f, 2.0f, 3, -1, x*x);
+  }
   constexpr double cos(double x)
   {
-    double sum = 1.0;
-    double prevsum = 0;
-    double n = 2.0;
-    double t = x * x;
-    double s = -1.0;
-    for (uint64_t i = 3; sum != prevsum; ++i)
-    {
-      prevsum = sum;
-      sum += s*t / n;
-      s = -s;
-      t *= x * x;
-      n *= i; ++i; n *= i;
-    }
-    return sum;
+    return detail::trig_series(x, 1.0, 2.0, 3, -1, x*x);
+  }
+  constexpr long double cos(long double x)
+  {
+    return detail::trig_series(x, 1.0l, 2.0l, 3, -1, x*x);
   }
 
+  //----------------------------------------------------------------------------
   // tan(x) = sin(x)/cos(x) - cos(x) cannot be 0
-  extern const char* tan_invoked_at_runtime;
+  // enforce that the domain is correct at compile-time (link-time)
+  extern const char* tan_domain_error;
+  constexpr float tan(float x)
+  {
+    return cos(x) != 0.0f ?
+      sin(x) / cos(x) :
+      throw std::domain_error(tan_domain_error);
+  }
   constexpr double tan(double x)
   {
-    double c = cos(x);
-    return c != 0 ?
-      sin(x)/c :
-      throw std::domain_error(tan_invoked_at_runtime);
+    return cos(x) != 0.0 ?
+      sin(x) / cos(x) :
+      throw std::domain_error(tan_domain_error);
+  }
+  constexpr long double tan(long double x)
+  {
+    return cos(x) != 0.0l ?
+      sin(x) / cos(x) :
+      throw std::domain_error(tan_domain_error);
   }
 
+  /*
   // floor and ceil: each works in terms of the other for negative numbers
   constexpr double ceil(double x);
 
@@ -141,23 +229,99 @@ namespace cex
   {
     return x * y;
   }
-
+  */
 }
 
-constexpr double PI = 3.14159265358979;
+//------------------------------------------------------------------------------
+// constexpr floating-point "equality" (within epsilon)
+template <typename T>
+constexpr bool feq(T x, T y)
+{
+  return cex::abs(x - y) <= std::numeric_limits<T>::epsilon();
+}
 
 int main(int, char* [])
 {
-  cout.precision(std::numeric_limits<double>::digits10);
+  static_assert(feq(1.0f, cex::abs(-1.0f)), "abs(-1.0f)");
+  static_assert(feq(1.0f, cex::abs(1.0f)), "abs(1.0f)");
+  static_assert(feq(1.0, cex::abs(-1.0)), "abs(-1.0)");
+  static_assert(feq(1.0, cex::abs(1.0)), "abs(1.0)");
+  static_assert(feq(1.0l, cex::abs(-1.0l)), "abs(-1.0l)");
+  static_assert(feq(1.0l, cex::abs(1.0l)), "abs(1.0l)");
 
-  constexpr double sin_pi_2 = cex::sin(PI/2.0);
-  cout << "sin(PI/2) = " << sin_pi_2 << endl;
+  static_assert(feq(1.0f, cex::fabs(-1.0f)), "fabs(-1.0f)");
+  static_assert(feq(1.0f, cex::fabs(1.0f)), "fabs(-1.0f)");
+  static_assert(feq(1.0, cex::fabs(-1.0)), "fabs(-1.0)");
+  static_assert(feq(1.0, cex::fabs(1.0)), "fabs(1.0)");
+  static_assert(feq(1.0l, cex::fabs(-1.0l)), "fabs(-1.0l)");
+  static_assert(feq(1.0l, cex::fabs(1.0l)), "fabs(1.0l)");
+
+  // square root of 2 = 1.414213562373095048802
+  static_assert(feq(1.0f, cex::sqrt(1.0f)), "sqrt(1.0f)");
+  static_assert(feq(1.4142136f, cex::sqrt(2.0f)), "sqrt(2.0f)");
+  static_assert(feq(1.0, cex::sqrt(1.0)), "sqrt(1.0)");
+  static_assert(feq(1.414213562373095, cex::sqrt(2.0)), "sqrt(2.0)");
+  static_assert(feq(1.0l, cex::sqrt(1.0l)), "sqrt(1.0l)");
+  static_assert(feq(1.4142135623730950488l, cex::sqrt(2.0l)), "sqrt(2.0l)");
+
+  // cube root of 2 = 1.259921049894873164767
+  static_assert(feq(1.0f, cex::cbrt(1.0f)), "cbrt(1.0f)");
+  static_assert(feq(1.259921f, cex::cbrt(2.0f)), "cbrt(2.0f)");
+  static_assert(feq(1.0, cex::cbrt(1.0)), "cbrt(1.0)");
+  static_assert(feq(1.259921049894873, cex::cbrt(2.0)), "cbrt(2.0)");
+  static_assert(feq(1.0l, cex::cbrt(1.0l)), "cbrt(1.0l)");
+  static_assert(feq(1.2599210498948731648l, cex::cbrt(2.0l)), "cbrt(2.0l)");
+
+  // e = 2.718281828459045235360
+  static_assert(feq(2.718282f, cex::exp(1.0f)), "exp(1.0f)");
+  static_assert(feq(2.7182818284590454, cex::exp(1.0)), "exp(1.0)");
+  static_assert(feq(2.7182818284590452354l, cex::exp(1.0l)), "exp(1.0l)");
+
+  // pi = 3.141592653589793238463
+  constexpr float PIf = 3.141593f;
+  constexpr double PI = 3.141592653589793;
+  constexpr long double PIl = 3.1415926535897932385l;
+
+  static_assert(feq(0.0f, cex::sin(PIf)), "sin(PIf)");
+  static_assert(feq(0.0, cex::sin(PI)), "sin(PI)");
+  static_assert(feq(0.0l, cex::sin(PIl)), "sin(PIl)");
+
+  // pi/2 = 1.570796326794896619231
+  constexpr float PI2f = 1.57079633f;
+  constexpr double PI2 = 1.5707963267948966;
+  constexpr long double PI2l = 1.5707963267948966192l;
+
+  static_assert(feq(1.0f, cex::sin(PI2f)), "sin(PI/2f)");
+  static_assert(feq(1.0, cex::sin(PI2)), "sin(PI/2)");
+  static_assert(feq(1.0l, cex::sin(PI2l)), "sin(PI/2l)");
+
+  static_assert(feq(1.0f, cex::cos(0.0f)), "cos(0f)");
+  static_assert(feq(1.0, cex::cos(0.0)), "cos(0)");
+  static_assert(feq(1.0l, cex::cos(0.0l)), "cos(0l)");
+  static_assert(feq(0.0f, cex::cos(PI2f)), "cos(PI/2f)");
+  static_assert(feq(0.0, cex::cos(PI2)), "cos(PI/2)");
+  static_assert(feq(0.0l, cex::cos(PI2l)), "cos(PI/2l)");
+
+  static_assert(feq(0.0f, cex::tan(0.0f)), "tan(0f)");
+  static_assert(feq(0.0, cex::tan(0.0)), "tan(0)");
+  static_assert(feq(0.0l, cex::tan(0.0l)), "tan(0l)");
+
+  // pi/4 = 0.785398163397448309615
+  constexpr float PI4f = 0.78539816f;
+  constexpr double PI4 = 0.7853981633974483;
+  constexpr long double PI4l = 0.78539816339744830962l;
+
+  static_assert(feq(1.0f, cex::tan(PI4f)), "tan(PI/4f)");
+  static_assert(feq(1.0, cex::tan(PI4)), "tan(PI/4)");
+  static_assert(feq(1.0l, cex::tan(PI4l)), "tan(PI/4l)");
+
+  /*
+
+
+
 
   constexpr double cos_pi_4 = cex::cos(PI/4.0);
   cout << "cos(PI/4) = " << cos_pi_4 << endl;
-
-  constexpr double sqrt_2 = cex::sqrt(2.0);
-  cout << "sqrt(2) = " << sqrt_2 << endl;
 
   constexpr double e = cex::exp(3.0);
   cout << "e = " << e << endl;
@@ -184,4 +348,5 @@ int main(int, char* [])
   cout << "ceil(-3.2) = " << ceil_neg_32 << endl;
 
   cout << "Hello, world!" << endl;
+  */
 }
