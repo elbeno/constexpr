84270205a4211a5dd485441a22db32efc41f5786
Ben Deane <ben@elbeno.com>
2015-08-23 20:04:21 -0700

    Add fmod

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -329,6 +329,27 @@ namespace cex
     }
   }
   #endif
+
+  //----------------------------------------------------------------------------
+  // mod and fmod: floating-point remainder functions
+  const char* mod_domain_error;
+  constexpr float fmod(float x, float y)
+  {
+    return y != 0 ? x - floor(x/y)*y :
+      throw std::domain_error(mod_domain_error);
+  }
+  constexpr double fmod(double x, double y)
+  {
+    return y != 0 ? x - floor(x/y)*y :
+      throw std::domain_error(mod_domain_error);
+  }
+  #if __cplusplus == 201402L
+  constexpr long double fmod(long double x, long double y)
+  {
+    return y != 0 ? x - floor(x/y)*y :
+      throw std::domain_error(mod_domain_error);
+  }
+  #endif
 }
 
 //------------------------------------------------------------------------------
@@ -341,6 +362,8 @@ constexpr bool feq(T x, T y)
 
 int main(int, char* [])
 {
+  //----------------------------------------------------------------------------
+  // abs
   static_assert(feq(1.0f, cex::abs(-1.0f)), "abs(-1.0f)");
   static_assert(feq(1.0f, cex::abs(1.0f)), "abs(1.0f)");
   static_assert(feq(1.0, cex::abs(-1.0)), "abs(-1.0)");
@@ -348,6 +371,8 @@ int main(int, char* [])
   static_assert(feq(1.0l, cex::abs(-1.0l)), "abs(-1.0l)");
   static_assert(feq(1.0l, cex::abs(1.0l)), "abs(1.0l)");
 
+  //----------------------------------------------------------------------------
+  // fabs
   static_assert(feq(1.0f, cex::fabs(-1.0f)), "fabs(-1.0f)");
   static_assert(feq(1.0f, cex::fabs(1.0f)), "fabs(-1.0f)");
   static_assert(feq(1.0, cex::fabs(-1.0)), "fabs(-1.0)");
@@ -355,9 +380,13 @@ int main(int, char* [])
   static_assert(feq(1.0l, cex::fabs(-1.0l)), "fabs(-1.0l)");
   static_assert(feq(1.0l, cex::fabs(1.0l)), "fabs(1.0l)");
 
+  //----------------------------------------------------------------------------
+  // ipow
   static_assert(feq(9.0f, cex::ipow(3.0f, 2)), "ipow(3.0f, 2)");
   static_assert(feq(0.111111f, cex::ipow(3.0f, -2)), "ipow(3.0f, -2)");
 
+  //----------------------------------------------------------------------------
+  // sqrt
   // square root of 2 = 1.414213562373095048802
   static_assert(feq(1.0f, cex::sqrt(1.0f)), "sqrt(1.0f)");
   static_assert(feq(1.4142136f, cex::sqrt(2.0f)), "sqrt(2.0f)");
@@ -366,6 +395,8 @@ int main(int, char* [])
   static_assert(feq(1.0l, cex::sqrt(1.0l)), "sqrt(1.0l)");
   static_assert(feq(1.4142135623730950488l, cex::sqrt(2.0l)), "sqrt(2.0l)");
 
+  //----------------------------------------------------------------------------
+  // cbrt
   // cube root of 2 = 1.259921049894873164767
   static_assert(feq(1.0f, cex::cbrt(1.0f)), "cbrt(1.0f)");
   static_assert(feq(1.259921f, cex::cbrt(2.0f)), "cbrt(2.0f)");
@@ -374,11 +405,15 @@ int main(int, char* [])
   static_assert(feq(1.0l, cex::cbrt(1.0l)), "cbrt(1.0l)");
   static_assert(feq(1.2599210498948731648l, cex::cbrt(2.0l)), "cbrt(2.0l)");
 
+  //----------------------------------------------------------------------------
+  // exp
   // e = 2.718281828459045235360
   static_assert(feq(2.718282f, cex::exp(1.0f)), "exp(1.0f)");
   static_assert(feq(2.7182818284590454, cex::exp(1.0)), "exp(1.0)");
   static_assert(feq(2.7182818284590452354l, cex::exp(1.0l)), "exp(1.0l)");
 
+  //----------------------------------------------------------------------------
+  // sin
   // pi = 3.141592653589793238463
   constexpr float PIf = 3.141593f;
   constexpr double PI = 3.141592653589793;
@@ -397,6 +432,8 @@ int main(int, char* [])
   static_assert(feq(1.0, cex::sin(PI2)), "sin(PI/2)");
   static_assert(feq(1.0l, cex::sin(PI2l)), "sin(PI/2l)");
 
+  //----------------------------------------------------------------------------
+  // cos
   static_assert(feq(1.0f, cex::cos(0.0f)), "cos(0f)");
   static_assert(feq(1.0, cex::cos(0.0)), "cos(0)");
   static_assert(feq(1.0l, cex::cos(0.0l)), "cos(0l)");
@@ -404,6 +441,8 @@ int main(int, char* [])
   static_assert(feq(0.0, cex::cos(PI2)), "cos(PI/2)");
   static_assert(feq(0.0l, cex::cos(PI2l)), "cos(PI/2l)");
 
+  //----------------------------------------------------------------------------
+  // tan
   static_assert(feq(0.0f, cex::tan(0.0f)), "tan(0f)");
   static_assert(feq(0.0, cex::tan(0.0)), "tan(0)");
   static_assert(feq(0.0l, cex::tan(0.0l)), "tan(0l)");
@@ -417,6 +456,8 @@ int main(int, char* [])
   static_assert(feq(1.0, cex::tan(PI4)), "tan(PI/4)");
   static_assert(feq(1.0l, cex::tan(PI4l)), "tan(PI/4l)");
 
+  //----------------------------------------------------------------------------
+  // floor and ceil
   static_assert(cex::floor(PIf) == 3.0f, "floor(PIf)");
   static_assert(cex::ceil(PIf) == 4.0f, "ceil(PIf)");
   static_assert(cex::floor(-PIf) == -4.0f, "floor(-PIf)");
@@ -432,5 +473,13 @@ int main(int, char* [])
   static_assert(cex::ceil(PIl) == 4.0l, "ceil(PIl)");
   static_assert(cex::floor(-PIl) == -4.0l, "floor(-PIl)");
   static_assert(cex::ceil(-PIl) == -3.0l, "ceil(-PIl)");
+  #endif
+
+  //----------------------------------------------------------------------------
+  // fmod
+  static_assert(feq(1.0f, cex::fmod(9.0f, 4.0f)), "fmod(9.0f, 4.0f)");
+  static_assert(feq(1.0, cex::fmod(9.0, 4.0)), "fmod(9.0, 4.0)");
+  #if __cplusplus == 201402L
+  static_assert(feq(1.0l, cex::fmod(9.0l, 4.0l)), "fmod(9.0l, 4.0l)");
   #endif
 }
